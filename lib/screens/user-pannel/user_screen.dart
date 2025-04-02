59d638cd4ed11bd7c9dca4c50fcc2eeee6385cd0
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2025-04-02 22:12:35 +0600

    sale product added.

--- lib/screens/user-pannel/user_screen.dart
+++ lib/screens/user-pannel/user_screen.dart
@@ -2,7 +2,10 @@ import 'package:ecommerce_firebase/screens/auth-ui/welcome_screen.dart';
 import 'package:ecommerce_firebase/utils/colors.dart';
 import 'package:ecommerce_firebase/utils/strings.dart';
 import 'package:ecommerce_firebase/widgets/banner-widget.dart';
+import 'package:ecommerce_firebase/widgets/category-widget.dart';
 import 'package:ecommerce_firebase/widgets/custom-drawer-widget.dart';
+import 'package:ecommerce_firebase/widgets/flash-sale-widget.dart';
+import 'package:ecommerce_firebase/widgets/heading-widget.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -52,6 +55,20 @@ class _UserScreenState extends State<UserScreen> {
               height: Get.height / 90,
             ),
             const BannerWidget(),
+            HeadingWidget(
+              headingTitle: 'Categories',
+              headingSubTitle: 'Accroding to your budget',
+              onTap: () {},
+              buttonText: 'see more',
+            ),
+            const CategoriesWidget(),
+            HeadingWidget(
+              headingTitle: 'Flash Sale',
+              headingSubTitle: 'Accroding to your budget',
+              onTap: () {},
+              buttonText: 'see more',
+            ),
+            const FlashSaleWidget(),
           ],
         ),
       ),
